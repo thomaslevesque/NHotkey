dc1942e05b2b246f79a3238c3f12bfa93b73b4f2
Thomas Levesque <tom103@free.fr>
2014-01-23 01:56:37 +0100

    Commited missing files, incremented version

---
+++ NHotkey.Wpf/WeakReferenceCollection.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace NHotkey.Wpf
+{
+    class WeakReferenceCollection<T> : IEnumerable<T>
+        where T : class
+    {
+        private readonly List<WeakReference> _references = new List<WeakReference>();
+        
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (var reference in _references)
+            {
+                var target = reference.Target;
+                if (target != null)
+                    yield return (T) target;
+            }
+            Trim();
+        }
+
+        public void Add(T item)
+        {
+            _references.Add(new WeakReference(item));
+        }
+
+        public void Remove(T item)
+        {
+            _references.RemoveAll(r => (r.Target ?? item) == item);
+        }
+
+        public void Trim()
+        {
+            _references.RemoveAll(r => !r.IsAlive);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ NHotkey/NativeMethods.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace NHotkey
+{
+    static class NativeMethods
+    {
+        [DllImport("user32.dll", SetLastError = true)]
+        internal static extern bool RegisterHotKey(IntPtr hWnd, int id, HotkeyFlags fsModifiers, uint vk);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        internal static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+    }
+}
