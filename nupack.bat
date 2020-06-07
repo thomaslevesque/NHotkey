6dc5d42cbc137cad68337012cb3e7ad412df6856
Thomas Levesque <thomaslevesque@users.noreply.github.com>
2020-06-07 20:17:52 +0200

    Add missing files to solution
    Remove deprecated nuspec files

--- nupack.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set packageDir=NuGetPackages
-nuget pack -OutputDirectory %packageDir% %packageDir%\NHotkey.nuspec
-nuget pack -OutputDirectory %packageDir% %packageDir%\NHotkey.Wpf.nuspec
-nuget pack -OutputDirectory %packageDir% %packageDir%\NHotkey.WindowsForms.nuspec
